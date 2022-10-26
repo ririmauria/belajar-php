946c8a9c51915320d787a45e1de849298980fe09
Siti Zahra Mauria <ririan021@gmail.com>
2022-10-26 22:41:21 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+  echo "Hello World!<br>";
+
+  $nama = "Siti Zahra Mauria";
+  $umur = 20;
+
+  echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+  $NamaAdik = "Riri";
+  $UmurAdik =  15;
+
+  echo "Nama adik saya <strong>$NamaAdik</strong>, adik saya berusia$UmurAdik tahun.<br>";
+
+  $selisihumur = $umur - $UmurAdik;
+  echo "Selisih usia saya dan adik saya adalah $selisihumur tahun."
+?>
